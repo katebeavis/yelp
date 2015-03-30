9134c1abebbc30aab2b9f02592027adb19c5aba4
Kate <katebeavis84@gmail.com>
2015-03-30 16:56:53 +0100

    Maximum review rating of 5, all tests passing

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+describe Review, :type => :model do
+  it "is invalid if the rating is more than 5" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
+end
 end
